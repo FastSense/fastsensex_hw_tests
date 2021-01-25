07e2c1006aa5e07b5255e5830b081e2192b2c6a7
Volga <gonzalez1139@gmail.com>
2021-01-25 14:07:52 +0300

    Update usb_check script

--- usb_check.sh
+++ usb_check.sh
@@ -6,21 +6,12 @@ then
     exit 0
 fi
 
-while getopts m:d: flag 
+while getopts d: flag 
 do
     case "${flag}" in
-        m) MOUNT_POINT=${OPTARG};;
         d) DEVICE=${OPTARG};;
     esac
 done
-
-if [ -z "$MOUNT_POINT" ]
-then
-    MOUNT_POINT="/media/usb_test"
-    echo "Mount point not entered, default selected directory: $MOUNT_POINT"
-else
-    echo "Mount point: $MOUNT_POINT"
-fi
 
 if [ -z "$DEVICE" ]
 then
@@ -36,21 +27,14 @@ IFS=
 read -n 1 -s key
 if [ "$key" = "y" ] || [ "$key" = "Y" ]
 then
-
-    echo "Start USB testing"
-    mkdir $MOUNT_POINT
-    mount $DEVICE $MOUNT_POINT
+    echo "Start USB testing. Read from $DEVICE"
 
     if [ $? -eq 0 ]
     then
-        dd if=/dev/urandom of=$MOUNT_POINT/test_file bs=1M count=128 oflag=direct status=none
-        dd if=$MOUNT_POINT/test_file of=/dev/null bs=1M iflag=direct status=progress
-        rm $MOUNT_POINT/test_file
-        umount $DEVICE
+        dd if=$DEVICE of=/dev/null bs=1M count=1024 iflag=direct status=progress
     else
         echo "Something went wrong with the mount..."
     fi
-    rm -r $MOUNT_POINT
 
 else
     echo "Typed key: \"$key\". Exiting"

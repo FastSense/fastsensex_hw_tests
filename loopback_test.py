4d00e9d0397f3871cdb209087fd168c9f973c7f4
Volga <gonzalez1139@gmail.com>
2021-01-27 12:44:43 +0300

    Update loppback test, lopp com port selection, colorized pass/fail output, change default speed test packs number

--- loopback_test.py
+++ loopback_test.py
@@ -8,7 +8,7 @@ device_list = [('Conga-ma4', ma4_port_map), ('Conga-ma5', ma5_port_map)]
 
 # Parameters of the uart/usb loopback test
 # Number of passed packs
-usb_uart_packs = 1024
+usb_uart_packs = 128
 # Pack size in byte
 usb_uart_pack_size = 1024
 # Port baudrate
@@ -51,8 +51,9 @@ def device_select(device_list):
 def loopback_type_select():
     loopback_type = 0
     print('Chose lopback test type:')
-    print('    USB/UART: 1. To test port speed')
-    print('    Simple: 2. To check if the port is working')
+    print('    Simple: 1. To check if the port is working')
+    print('    USB/UART: 2. To test port speed')
+
 
     while not loopback_type:
         try:
@@ -78,6 +79,7 @@ def com_port_select(devise_name, port_map):
     print("Selected COM module version: {}. Select the com port to be tested:".format(devise_name))
     for port_id in range(len(port_map)):
         print("    COM{}({}): {}".format(port_id, port_map[port_id], port_id + 1))
+    print("    Exit: q")
 
     while not com_port:
         try:
@@ -98,18 +100,24 @@ def com_port_select(devise_name, port_map):
             continue
 
 
-def simple_loopback(com_port, com_speed, packet_number=1024, packet_size=1):
+def simple_loopback(com_port_name, com_speed, packet_number=1024, packet_size=1):
     checked_packs = 0
     corrupted_packs = 0
+    timeout_counter = 0
 
     try:
-        with serial.Serial(com_port, com_speed, timeout=1) as com_port:
+        with serial.Serial(com_port_name, com_speed, timeout=1) as com_port:
             print("\nPacks statistic:")
             for pack_id in range(packet_number):
                 pack = os.urandom(packet_size)
                 com_port.write(pack)
 
                 read_data = com_port.read(packet_size)
+
+                if len(read_data) != packet_size:
+                    timeout_counter += 1
+                if timeout_counter >= 10:
+                    break
 
                 if read_data == pack:
                     checked_packs += 1
@@ -118,7 +126,16 @@ def simple_loopback(com_port, com_speed, packet_number=1024, packet_size=1):
                 com_port.reset_input_buffer()
 
                 print("\rPassed: {}, checked: {}, corrupted {}".format(pack_id, checked_packs, corrupted_packs), end='')
+
             print()
+            if checked_packs == pack_id + 1:
+                print("Speed loopback test. Port: {}. Pass".format(com_port_name))
+                print("\33[32mPass\33[0m")
+            else:
+                print("Speed loopback test. Port: {}. Failed".format(com_port_name))
+                if timeout_counter >= 10:
+                    print("Timeout exit")
+                print("\33[31mFailed\33[0m")
 
     except serial.serialutil.SerialException as serial_fail:
         print(serial_fail)
@@ -126,19 +143,25 @@ def simple_loopback(com_port, com_speed, packet_number=1024, packet_size=1):
         sys.exit()
 
 
-def speed_loopback(com_port, usb_port, speed, packet_number=1024, packet_size=1024):
+def speed_loopback(com_port_name, usb_port_name, speed, packet_number=1024, packet_size=1024):
     checked_packs = 0
     corrupted_packs = 0
+    timeout_counter = 0
 
     try:
-        with serial.Serial(com_port, speed, timeout=1) as com_port, \
-             serial.Serial(usb_port, speed, timeout=1) as usb_port:
+        with serial.Serial(com_port_name, speed, timeout=1) as com_port, \
+             serial.Serial(usb_port_name, speed, timeout=1) as usb_port:
             print("Packs statistic:")
             for pack_id in range(packet_number):
                 pack = bytearray(os.urandom(packet_size))
                 com_port.write(pack)
 
                 usb_read_pack = bytearray(usb_port.read(packet_size))
+
+                if len(usb_read_pack) != packet_size:
+                    timeout_counter += 1
+                if timeout_counter >= 10:
+                    break
 
                 for byte in range(len(pack)):
                     if pack[byte] >= 255:
@@ -164,7 +187,17 @@ def speed_loopback(com_port, usb_port, speed, packet_number=1024, packet_size=10
                 usb_port.reset_input_buffer()
 
                 print("\rPassed: {}, checked: {}, corrupted {}".format(pack_id + 1, checked_packs, corrupted_packs), end='')
+
             print()
+            if checked_packs == pack_id + 1:
+                print("Speed loopback test. Port: {}.".format(com_port_name))
+                print("\33[32mPass\33[0m")
+            else:
+                print("Speed loopback test. Port: {}. Failed".format(com_port_name))
+                if timeout_counter >= 10:
+                    print("Timeout exit")
+                print("\33[31mFailed\33[0m")
+
 
     except serial.serialutil.SerialException as serial_fail:
         print(serial_fail)
@@ -173,15 +206,23 @@ def speed_loopback(com_port, usb_port, speed, packet_number=1024, packet_size=10
 
 
 if __name__ == "__main__":
+    finish = False
+
     device, port_map = device_select(device_list)
 
     loopback_type = loopback_type_select()
 
-    com_port = com_port_select(device, port_map)
+    while not finish:
+        com_port = com_port_select(device, port_map)
 
-    print("Selected device: {}, tested port {}".format(device, com_port))
+        print("Selected device: {}, tested port {}".format(device, com_port))
 
-    if loopback_type == 1:
-        speed_loopback(com_port, '/dev/ttyUSB0', usb_uart_speed, packet_number=usb_uart_packs, packet_size=usb_uart_pack_size)
-    elif loopback_type == 2:
-        simple_loopback(com_port, simple_speed, packet_number=simple_packs, packet_size=simple_pack_size)
+        if loopback_type == 1:
+            simple_loopback(com_port, simple_speed, packet_number=simple_packs, packet_size=simple_pack_size)
+        elif loopback_type == 2:
+            speed_loopback(com_port, '/dev/ttyUSB0', usb_uart_speed, packet_number=usb_uart_packs, packet_size=usb_uart_pack_size)
+
+        print("Enter 'q' to exit or another button to select port\n")
+        key = input()
+        if key == "q":
+            finish = True

ec59a47f419144fc3a904f3af9bb016f1aac30bd
Volga <gonzalez1139@gmail.com>
2021-03-02 12:44:06 +0300

    Rework stress test for vpu and tpu, add test for cpu

---
+++ telemetry/stress_tests/cpu_test.py
@@ -0,0 +1,26 @@
+import psutil
+import subprocess
+import time
+import sys
+
+max_stress_time = 60 * 10
+
+warn_temp = 50
+critical_temp = 60
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    with subprocess.Popen("stress --cpu %d" % psutil.cpu_count(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as stress_process:
+        while time.time() < start_time + max_stress_time:
+            temp = [temp.current for temp in psutil.sensors_temperatures()['coretemp'] if 'CORE' in temp.label.upper()]
+
+            for core_temp in temp:
+                if core_temp > warn_temp:
+                    pass
+                elif core_temp > critical_temp:
+                    print("\nWarn temperature. Exit")
+                    sys.exit()
+            print(f"CPU temperature: {temp}\r", end='')
+            time.sleep(0.5)
+        print("\nTemperature is stable, CPU test will passed")
--- telemetry/stress_tests/vpu_tpu_test.py
+++ telemetry/stress_tests/vpu_tpu_test.py
@@ -1,12 +1,20 @@
 import nnio
 import requests
 import time
+import sys
+import subprocess
+from pathlib import Path
+from threading import Thread
+from queue import Queue
 from openvino.inference_engine import IECore
 
 
-URL_MODEL_BIN = 'https://github.com/FastSense/nnio/raw/development/models/openvino/ssd_mobilenet_v2_coco/ssd_mobilenet_v2_coco_fp16.bin'
-URL_MODEL_XML = 'https://github.com/FastSense/nnio/raw/development/models/openvino/ssd_mobilenet_v2_coco/ssd_mobilenet_v2_coco_fp16.xml'
+max_stress_time = 60 * 10
+
 URL_TEST_IMAGE = 'https://raw.githubusercontent.com/FastSense/nnio/master/tests/dogs.jpg'
+myriad_temp = 0
+coral_temp = 0
+
 
 def system_check(iecore):
     #iecore.get_metric()
@@ -14,39 +22,88 @@ def system_check(iecore):
     return True
 
 
-def prepare_network():
-    # Load models
-    model = nnio.zoo.openvino.detection.SSDMobileNetV2("MYRIAD")
+class OpenvinoThread(Thread):
+    def __init__(self):
+        super().__init__()
+        self.temperature = 0
+        self.start_time = time.time()
+        self.daemon = True
 
-    # Get preprocessing function
-    preproc = model.get_preprocessing()
+        self.warning_temp = 80
+        self.critical_temp = 100
 
-    image_prepared = preproc('dogs.jpg')
+        self.device = "MYRIAD"
 
-    return image_prepared, model
+    def run(self):
+        openvino_model = nnio.zoo.openvino.detection.SSDMobileNetV2(self.device)
+        openvino_preproc = openvino_model.get_preprocessing()
+        openvino_image = openvino_preproc('dogs.jpg')
+
+        while time.time() < start_time + max_stress_time:
+            boxes, info = openvino_model(openvino_image, return_info=True)
+            self.temperature = openvino_model.model.ie.get_metric(metric_name="DEVICE_THERMAL", device_name="MYRIAD")
+            if self.temperature > warning_temp:
+                print("Warning {}".format(warning_temp))
+            elif self.temperature > critical_temp:
+                print("Critical {}".format(critical_temp))
+                break
 
 
-def inference(preproc, model):
-    boxes, info = model(image_prepared, return_info=True)
-    print(info.temperature)
+class TFliteThread(Thread):
+    def __init__(self):
+        super().__init__()
+        self.temperature = 0
+        self.start_time = time.time()
+        self.daemon = True
+
+        self.warning_temp = 80
+        self.critical_temp = 100
+
+        self.device = "TPU"
+
+    def run(self):
+        tflite_model = nnio.zoo.edgetpu.detection.SSDMobileNet(self.device)
+        tflite_preproc = tflite_model.get_preprocessing()
+        tflite_image = tflite_preproc('dogs.jpg')
+
+        device_id = 0
+        coral_temp_path = Path(f"/sys/class/apex/apex_{device_id}/temp")
+
+        with open(coral_temp_path, 'r') as coral_temp:
+            while time.time() < start_time + max_stress_time:
+                boxes, info = tflite_model(tflite_image, return_info=True)
+                self.temperature = coral_temp.readline()
+                coral_temp.seek(0)
+                self.temperature = int(self.temperature) / 1000
+
+                if self.temperature > warning_temp:
+                    print("Warning {}".format(warning_temp))
+                elif self.temperature > critical_temp:
+                    print("Critical {}".format(critical_temp))
+                    break
 
 
 if __name__ == '__main__':
-    max_stress_time = 60 * 10
-    start_time = time.time()
-
     critical_temp = 100
     warning_temp = 80
 
+    start_time = time.time()
+
+
     img_data = requests.get(URL_TEST_IMAGE).content
-    with open('dog.jpg', 'wb') as image_file:
+    with open('dogs.jpg', 'wb') as image_file:
         image_file.write(img_data)
 
-    image, net_model = prepare_network()
-    while time.time() < start_time + max_stress_time:
-        boxes, info = net_model(image, return_info=True)
-        print("Intel Myriad. Temp:{:.2f}\r".format(info["temperature"]), end='')
-        if info["temperature"] > warning_temp:
-            print("Warning {}".format(warning_temp))
-        elif info["temperature"] > critical_temp:
-            print("Critical {}".format(critical_temp))
+    openvino_thread = OpenvinoThread()
+    tflite_thread = TFliteThread()
+    #openvino_thread.start()
+    tflite_thread.start()
+
+    try:
+        while time.time() < start_time + max_stress_time:
+            print(f"Myriad temp: {openvino_thread.temperature:.2f}. Coral temp: {coral_temp}\r", end='')
+            time.sleep(0.5)
+    except:
+        sys.exit()
+
+    print("Pass")
